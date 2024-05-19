2fe51d2fac9074a0dbc438af7705b52442820e18
ddangeun123 <213jung@naver.com>
2024-05-19 17:01:07 +0900

    Refactor scraper driver quit logic and manage Chrome processes

--- naver/app/app.py
+++ naver/app/app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
+import subprocess
+import platform
 
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
@@ -13,6 +15,16 @@ from scraper import Scraper
 
 app = FastAPI()
 executor = ThreadPoolExecutor(max_workers=4)
+def get_os_info():
+    os_info = platform.system()
+    if os_info == 'Windows':
+        return 'Windows'
+    elif os_info == 'Linux':
+        return 'Linux'
+    elif os_info == 'Darwin':
+        return 'Mac'
+cur_os = get_os_info()
+enable_chromes = []
 
 def fetch(url):
     driver = webdriver.Chrome()
@@ -57,7 +69,9 @@ async def search_naver(keywords: str):
 def naver_shopping_task(keywords: str):
     try:
         scraper = Scraper()
+        get_enable_pid(scraper.driver)
         result = scraper.scrape_navershopping(keywords)
+        remove_enable_pid(scraper.driver)
         return result
     except Exception as e:
         traceback.print_exc()
@@ -65,6 +79,7 @@ def naver_shopping_task(keywords: str):
     finally:
         if scraper.driver != None:
             scraper.driver.quit()
+        chrome_manage(cur_os)
 
 @app.get("/search/navershopping")
 async def search_naver_shopping(keywords: str):
@@ -81,3 +96,34 @@ async def search_naver_shopping(keywords: str):
     except Exception as e:
         traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
+
+def get_enable_pid(driver):
+    pid = driver.service.process.pid
+    enable_chromes.append(pid)
+def remove_enable_pid(driver):
+    pid = driver.service.process.pid
+    enable_chromes.remove(pid)
+    
+def chrome_manage(os:str):
+    if os == 'Windows':
+        list_command = 'tasklist /FI "IMAGENAME eq chrome.exe"'
+        process = subprocess.Popen(["powershell", list_command], stdout=subprocess.PIPE)
+        output = process.communicate()[0].decode('utf-8')
+        lines = output.strip().split('\n')
+
+        current_pids = [line.split()[1] for line in lines[3:]]
+
+        kill_pids = [pid for pid in current_pids if int(pid) not in enable_chromes]
+
+        for pid in kill_pids:
+            subprocess.call('taskkill /F /PID {}'.format(pid), shell=True)
+    elif os == 'Linux' or os == 'Mac':
+        list_command = 'pgrep -f "Google Chrome"'
+        process = subprocess.Popen(list_command, shell=True, stdout=subprocess.PIPE)
+        output = process.communicate()[0].decode('utf-8')
+        current_pids = output.strip().split('\n')
+
+        kill_pids = [pid for pid in current_pids if int(pid) not in enable_chromes]
+
+        for pid in kill_pids:
+            subprocess.call('pkill -TERM -P {}'.format(pid), shell=True)
