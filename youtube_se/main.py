f780d788faf0ac679319483f0ba630eb78549834
ddangeun123 <213jung@naver.com>
2024-06-05 15:02:00 +0900

    Update port number for uvicorn.run in main.py files

--- youtube_se/main.py
+++ youtube_se/main.py
@@ -5,4 +5,4 @@ sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8002, reload=True)
