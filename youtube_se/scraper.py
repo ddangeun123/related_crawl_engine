4678daa6b0c0696527b3f3b3f65c7a2ed2a93fed
ddangeun123 <133068471+ddangeun123@users.noreply.github.com>
2024-06-12 15:36:13 +0900

    refactor: Update Scraper class to improve video detail extraction

--- youtube_se/scraper.py
+++ youtube_se/scraper.py
@@ -128,11 +128,13 @@ class Scraper:
         try:
             title = soup.find(class_='ytp-title').text
             description = soup.find(class_='ytd-expandable-video-description-body-renderer').text
+            view_count = soup.find('view-count-factoid-renderer').find(class_='YtwFactoidRendererValue').text
+            author = soup.find(id='upload-info').find(id='text-container').text
+            publish_date = soup.find(id='info-strings').text
             info = soup.find(id='info-conatiner').find_all('span')
             # view_count = info[0].text
             # publish_date = info[2].text
-            view_count = soup.find('view-count-factoid-renderer').find(class_='YtwFactoidRendererValue').text
-            author = soup.find(id='upload-info').find(id='text-container').text
+            
             
         except:
             traceback.print_exc()
